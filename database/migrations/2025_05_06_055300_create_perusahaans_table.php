20a9a5cbf25825f7b89ed3dbcb575f3387df0067
Galang Pakusadewa <galangpakusadewa007@gmail.com>
2025-05-06 15:15:29 +0700

    add migrations

---
+++ database/migrations/2025_05_06_055300_create_perusahaans_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('perusahaans', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('nama_perusahaan');
+            $table->string('logo');
+            $table->string('bidang_usaha');
+            $table->string('kalurahan');
+            $table->string('kecamatan');
+            $table->string('kabupaten');
+            $table->string('provinsi');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('perusahaans');
+    }
+};

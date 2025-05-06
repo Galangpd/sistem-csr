b5001a7cb26b3f3c7e62284dda3757ff497a24a0
Galang Pakusadewa <galangpakusadewa007@gmail.com>
2025-05-06 15:15:42 +0700

    add seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,7 +4,10 @@ namespace Database\Seeders;
 
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Masyarakat;
+use App\Models\Perusahaan;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +16,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        
+        User::create([
+            'name' => 'test user',
+            'username' => 'test123',
+            'password' => Hash::make('admin123'),
+            'role' => 'perusahaan',
+        ]);
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        User::factory(10)->create();
+        $this->call([
+            PerusahaanSeeder::class,
+            MasyarakatSeeder::class,
         ]);
     }
 }

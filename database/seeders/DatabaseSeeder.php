9e05de756fcac406d2787c017d67c9787bd1c864
Galang Pakusadewa <galangpakusadewa007@gmail.com>
2025-07-31 09:53:40 +0700

    update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -27,7 +27,7 @@ class DatabaseSeeder extends Seeder
             'status'       => 'approved',
             'verified_at'  => now(),
         ]);
-        User::factory(30)->create();
+        User::factory(10)->create();
         $this->call([
             BidangUsahaSeeder::class,
             JenisBantuanSeeder::class,
